8b5ffdbfa032b289ba196f1b33333932f116545a
ChloeConrad <90626378+ChloeConrad@users.noreply.github.com>
2021-11-24 19:22:38 +0100

    merge
    #MERGE

--- src/psycomp/Croisement.java
+++ src/psycomp/Croisement.java
@@ -1,4 +1,7 @@
 package psycomp;
+
+import java.util.Arrays;
+
 /**
  * Classe croisement représentant l'intersection de plusieurs rues, un croisement est représenté par une lettre et un nombre
  * @author Y.Cheriti et C.Conrad
@@ -6,11 +9,11 @@ package psycomp;
  */
 public class Croisement {
 	String nom;
-	Croisement[] voisin;
+	String[] voisin;
 	double[] distVoisin;
 	double distObj;
 	
-	public Croisement(String nom, Croisement[] voisin, double[] distVoisin, double distObj){
+	public Croisement(String nom, String[] voisin, double[] distVoisin, double distObj){
 		this.nom = nom;
 		this.voisin = voisin;
 		this.distVoisin = distVoisin;
@@ -19,10 +22,21 @@ public class Croisement {
 	public String getNom() {
 		return nom;
 	}
-	public void setNom(char[] nom) {
+	public void setNom(String nom) {
 		this.nom = nom;
 	}
 	public boolean equals(Croisement c) {
 		return (this.nom.equals(c.nom));
+	}
+	public boolean estVoisin(Croisement c) {
+		for(int i = 0; i<voisin.length;i++) {
+			if(voisin[i].equals(c.getNom()))
+				return true;
+		}
+		return false;
+	}
+	
+	public String toString() {
+		return nom +" "+ Arrays.toString(voisin)+" "+Arrays.toString(distVoisin)+" "+distObj;
 	}
 }
--- src/psycomp/Map.java
+++ src/psycomp/Map.java
@@ -1,9 +1,58 @@
 package psycomp;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.util.ArrayList;
 
 public class Map {
 	Croisement[] arbre;
-	public Map() {
-		// TODO Auto-generated
+	
+	public Map() throws IOException {
+		
+		Reader r = new FileReader("D:\\Users\\psychologie computationel\\Psychologie-Computationel\\src\\psycomp\\croisements.txt");
+		BufferedReader data = new BufferedReader(r);
+		String ligne = null;
+		int k = 0;
+		Croisement[] arbre = new Croisement[53];
+		while((ligne = data.readLine())!=null) {
+			String nom = ligne.substring(0, ligne.indexOf(" "));
+			String voisin = ligne.substring(ligne.indexOf("[")+1, ligne.indexOf("]")+1);
+			String distVoisin = ligne.substring(ligne.lastIndexOf("[")+1,ligne.lastIndexOf("]")+1);
+			double distBut =Float.parseFloat(ligne.substring(ligne.lastIndexOf("]")+2,ligne.length()));
+			
+			int nbrVoisin=1;
+			for(int i = 0;i<voisin.length();i++) {
+				if(voisin.charAt(i)==',')
+					nbrVoisin++;
+			}
+			
+			String[] listeVoisin = new String[nbrVoisin];
+			double[] listeDistVoisin = new double[nbrVoisin];
+			for(int i = 0; i<listeVoisin.length-1;i++) {
+				listeVoisin[i] = voisin.substring(0,voisin.indexOf(","));
+				listeDistVoisin[i] = Float.parseFloat(distVoisin.substring(0,distVoisin.indexOf(",")));
+				voisin = voisin.substring(voisin.indexOf(",")+1);
+				distVoisin = distVoisin.substring(distVoisin.indexOf(",")+1);
+			}
+			listeVoisin[listeVoisin.length-1]=voisin.substring(0,voisin.indexOf("]"));
+			listeDistVoisin[listeDistVoisin.length-1]=Float.parseFloat(distVoisin.substring(0,distVoisin.indexOf("]")));
+			arbre[k]=new Croisement(nom,listeVoisin,listeDistVoisin,distBut);
+			k++;
+			
+		}
+		this.arbre=arbre;
+		data.close();
+		
 	}
+	
+	public static void main(String[] args) throws IOException {
+		Map m = new Map();
+		System.out.println(m.arbre[52]);
+	}
+	
 
 }
