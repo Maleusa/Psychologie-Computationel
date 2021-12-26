4a237ca6e9e2dd19a2d8b9da83edbb14f6447d53
ChloeConrad <90626378+ChloeConrad@users.noreply.github.com>
2021-12-26 14:22:18 +0100

    merge
    merge

--- src/psycomp/estimAStarParam.java
+++ src/psycomp/estimAStarParam.java
@@ -36,9 +36,21 @@ public class estimAStarParam extends AStarParametrique{
 		return l;
 	}
 	
-	private static ArrayList dataProg(ArrayList<Croisements> l) {
+	private static ArrayList dataProg(ArrayList<Croisement> l) {
 		ArrayList<Double> lResu = new ArrayList<Double>();
-		
+		for(int i = 0; i<53;i++) {
+			boolean b = false;
+			for(int k = 0; k<l.size();k++) {
+				if(l.get(k).getNom().equals(i+"")) {
+					lResu.add(i,1.0);
+					b=true;
+				}
+				if(b==false) {
+					lResu.add(i,0.0);
+				}
+			}
+			return lResu;
+		}
 	}
 	
 	
@@ -47,12 +59,45 @@ public class estimAStarParam extends AStarParametrique{
 	 * de l'erreur d'estimation des humains sur le papiers Thorndyke, P. W. (1981). Distance estimation from cognitive maps. Cognitive Psychology, 13(4), 526‑550. https://doi.org/10.1016/0010-0285(81)90019-0
 	 */
 	public double fonctionEvaluation(double distParcourue, Croisement c1, Croisement c2, double k) {
-		double rand = randomNormal(7.8,1.54);
+		double rand = randomNormal(k2,ecartType);
 		distParcourue=distParcourue*k+rand*step;
 		//distParcourue=distParcourue*k1+(k2+(Math.random() * ((1.84 + 1.84 ) + 1)) - 1.84)*step;
 		if(!c2.estVoisin(c1))
 			throw new IllegalArgumentException();// ca risque pas d'etre bloquant si on fait le parcours en automatique?
 		return distParcourue+c1.distVoisin(c2)+c2.getDistObj();
+	}
+	
+	public ArrayList<Croisement> parcours(){
+		
+		if(depart.estVoisin(arrivee)) {
+			chemin.add(arrivee);
+			return chemin;
+		}
+		String[] s = depart.getVoisin();
+		double minFonctionEval = Integer.MAX_VALUE;
+		double fonctionEval;
+		
+		int indexMin=1;
+		for(int i = 0; i<s.length;i++) {
+			Croisement c = m.getCroisement(s[i]);
+			fonctionEval = fonctionEvaluation(distParcourue,depart,c,k1);
+			
+			if(!chemin.contains(c)) {
+				if(minFonctionEval>fonctionEval) {
+					minFonctionEval = fonctionEval;
+					indexMin = i;
+					step=step+1;
+				}
+			}
+		}
+		
+		Croisement etape =  m.getCroisement(s[indexMin]);
+		System.out.println("etape = " + etape);
+		System.out.println("depart = "+ depart);
+		distParcourue += etape.distVoisin(depart);
+		this.depart = etape;
+		chemin.add(etape);
+		return parcours();
 	}
 
 	
